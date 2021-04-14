ff6aa34d80756e0469fd0d5d47d38984cb52a53f
Byron Lemus <blemus@applaudostudios.com>
2021-04-13 21:42:55 -0600

    first commit

---
+++ src/component/hearder/hearder.tsx
@@ -0,0 +1,10 @@
+import React, { FC, useContext, useEffect, useReducer, useState } from 'react';
+
+const Header: FC = () =>{
+    return(
+       <>
+       </>
+    );
+}
+
+export default Header;
